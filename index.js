ad3273c8d8790fb125c25d0da848e89f12b91c8e
kheith01 <kheithb@gmail.com>
2021-01-09 21:20:12 +0300

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const Datastore = require('nedb');
 
@@ -8,3 +9,25 @@ database.loadDatabase();
 
 app.listen(3000, ()=> console.log('listening to port 3000'))
 app.use(express.static('public'));
+app.use(express.json({limit:'1mb'}))
+
+app.post('/data', (request, response)=>{
+    console.log(request.body);
+    const datainfo = request.body;
+    const timestamp = Date.now();
+    datainfo.timestamp = timestamp;
+    database.insert(datainfo);
+    response.json({data: 'delivered'})
+})
+
+app.get('/dataGet', (request, response)=>{
+    database.find({}, (err, data)=>{
+        if(err){
+            response.end
+            return;
+        }else{
+            response.json(data);
+        }
+    })
+    
+});
